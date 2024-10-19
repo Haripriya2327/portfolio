31283ceadc5cf0407aca9102fbeb9f73f28f66c3
Pravin S <you@example.com>
2024-10-20 01:45:43 +0200

    download relative path

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Download = () => {
     return (
         <div>
-            <a href="/hari_cv.pdf" download="hari_cv.pdf">
+            <a href="../hari_cv.pdf" download="hari_cv.pdf">
                 <button>Download Resume</button>
             </a>
         </div>
